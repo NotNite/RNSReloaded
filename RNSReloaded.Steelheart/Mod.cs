8f01a1a7c68cb15edbb3c45f465713bf8fa5b401
Alice <fuzzything44@gmail.com>
2024-05-25 15:14:50 -0700

    Steelheart progress Fights going to enrage should be fully functional now, including shira and matti

--- RNSReloaded.Steelheart/Mod.cs
+++ RNSReloaded.Steelheart/Mod.cs
@@ -14,20 +14,31 @@ public unsafe class Mod : IMod {
     private IHook<ScriptDelegate>? damageHook;
     private IHook<ScriptDelegate>? enrageHook;
     private IHook<ScriptDelegate>? newFightHook;
+    // Need to disable enrage after phase change
     private IHook<ScriptDelegate>? phaseChangeHook;
-    private IHook<ScriptDelegate>? bossStartHook;
-
+    // For shira phase 1
+    private IHook<ScriptDelegate>? shiraStartHook;
+    // For Shira unavoidable steel yourself attacks
+    private IHook<ScriptDelegate>? steelHook;
+    private IHook<ScriptDelegate>? steelActivateHook;
     // Flag to track if the enemy has started enrage
     // Set to false on combat start and phase change (scrdt_encounter, scrbp_boss_heal)
     // Set to true on enrage (scrbp_make_warning_enrage) (actual enrage pattern not used by bosses)
     private bool enraged = false;
-    // Flag to see if this is phase 1 of a boss, since boss phase transition needs to be prevented
-    // Set to false on combat start and phase change (same as above)
-    // Set to true when boss music starts (scr_music_play_boss_track) - note that this seems to be integrated
-    // into the track, so using Steel Yourself to force an encounter won't enable this flag.
-    // Also might not work on shira, so ideally a better enable method could be found.
-    // Maybe look into scrbp_health_threshold?
-    private bool isBossP1 = false;
+    // Name of the current encounter, so we can check vs. boss encounters to see where to pause HP at.
+    private string currentEncounter = "";
+    private static readonly Dictionary<string, double> BossPhaseChanges = new Dictionary<string, double>() {
+        { "enc_rabbit_queen0", 0.3 },
+        { "enc_dragon_mythril0", 0.4 },
+        { "enc_bird_valedictorian0", 0.4 },
+        { "enc_frog_idol0", 0.4 },
+        { "enc_mouse_paladin0", 0.4 },
+        { "enc_wolf_steeltooth0", 0.4 },
+    };
+
+    // Counts how many times Shira has used her steel yourself attack. Note that the script is 
+    // called twice per time she attacks. So 1/2 = first attack, 3/4 = 2nd, 5/6 = 3rd
+    private int shiraSteelCount = 0;
 
     public void Start(IModLoaderV1 loader) {
         this.rnsReloadedRef = loader.GetController<IRNSReloaded>();
@@ -49,47 +60,45 @@ public unsafe class Mod : IMod {
         ) {
             rnsReloaded.LimitOnlinePlay();
 
-            var damageId = rnsReloaded.ScriptFindId("scr_pattern_deal_damage_enemy_subtract");
-            var damageScript = rnsReloaded.GetScriptData(damageId - 100000);
-
+            var damageScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("scr_pattern_deal_damage_enemy_subtract") - 100000);
             this.damageHook =
                 hooks.CreateHook<ScriptDelegate>(this.EnemyDamageDetour, damageScript->Functions->Function);
             this.damageHook.Activate();
             this.damageHook.Enable();
 
-            var enrageId = rnsReloaded.ScriptFindId("scrbp_make_warning_enrage");
-            var enrageScript = rnsReloaded.GetScriptData(enrageId - 100000);
+            var enrageScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("scrbp_make_warning_enrage") - 100000);
             this.enrageHook =
                 hooks.CreateHook<ScriptDelegate>(this.EnrageDetour, enrageScript->Functions->Function);
             this.enrageHook.Activate();
             this.enrageHook.Enable();
 
-            var newFightId = rnsReloaded.ScriptFindId("scrdt_encounter");
-            var newFightScript = rnsReloaded.GetScriptData(newFightId - 100000);
+            var newFightScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("scrdt_encounter") - 100000);
             this.newFightHook =
                 hooks.CreateHook<ScriptDelegate>(this.NewFightDetour, newFightScript->Functions->Function);
             this.newFightHook.Activate();
             this.newFightHook.Enable();
 
-            var phaseChangeId = rnsReloaded.ScriptFindId("scrbp_boss_heal");
-            var phaseChangeScript = rnsReloaded.GetScriptData(phaseChangeId - 100000);
+            var phaseChangeScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("scrbp_boss_heal") - 100000);
             this.phaseChangeHook =
                 hooks.CreateHook<ScriptDelegate>(this.PhaseChangeDetour, phaseChangeScript->Functions->Function);
             this.phaseChangeHook.Activate();
             this.phaseChangeHook.Enable();
 
-            var bossStartId = rnsReloaded.ScriptFindId("scr_music_play_boss_track");
-            var bossStartScript = rnsReloaded.GetScriptData(bossStartId - 100000);
-            this.bossStartHook =
-                hooks.CreateHook<ScriptDelegate>(this.BossStartDetour, bossStartScript->Functions->Function);
-            this.bossStartHook.Activate();
-            this.bossStartHook.Enable();
+            var steelScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("bp_rabbit_queen1_steel") - 100000);
+            this.steelHook =
+                hooks.CreateHook<ScriptDelegate>(this.SteelDetour, steelScript->Functions->Function);
+            this.steelHook.Activate();
+            this.steelHook.Enable();
 
-            // TODO: make this work with matti mice (need some flag when they get summoned, remove it at end of fight)
-            // TODO: make this work with shira (investigation needed for shira defense required move/patterns in general)
+            var steelAScript = rnsReloaded.GetScriptData(rnsReloaded.ScriptFindId("bp_rabbit_queen1_steel_activate") - 100000);
+            this.steelActivateHook =
+                hooks.CreateHook<ScriptDelegate>(this.SteelActivateDetour, steelAScript->Functions->Function);
+            this.steelActivateHook.Activate();
+            this.steelActivateHook.Enable();
 
             // TODO (long term): Disable iFrames except from taking damage
-            // - Look into wolf boss time slow code for hints
+            // - Look into wolf boss time slow code for hints (maybe bp_invulncancel)
+            // - Look into scr_player_invuln
         }
     }
 
@@ -122,18 +131,20 @@ public unsafe class Mod : IMod {
     private RValue* EnemyDamageDetour(
         CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
     ) {
-        // Any enraged enemy is instantly killable
-        if (!this.enraged) {
+        // If enraged or matti mice summons, then skip the HP locking
+        if (!this.enraged && !(this.currentEncounter == "enc_mouse_paladin1" && argv[1]->Real > 0)) {
             double enemyHp = this.GetEnemyHP(argv[1]->Real);
             double enemyMaxHp = this.GetEnemyMaxHP(argv[1]->Real);
-            // Bosses phase change ~40% HP
-            // So to force p1 to go to enrage, we put a check there instead of at 250 HP
-            if (this.isBossP1 && (enemyHp - argv[2]->Real) < (enemyMaxHp * 0.4 + 250)) {
-                argv[2]->Real = Math.Max((enemyHp - enemyMaxHp * .4) - 250, 0);
-            // Non bosses (and boss phase 2s) can go to 250 HP before waiting on enrage to be killable
-            // 250 instead of 1 to hopefully make the bug where sometimes this lock could be broken
-            // by enough damage instances in the same frame.
+            if (BossPhaseChanges.TryGetValue(this.currentEncounter, out var hpThreshold)) {
+                // Bosses phase change higher HP (depends on the boss)
+                // So to force p1 to go to enrage, we put a check there instead of at 250 HP
+                if ((enemyHp - argv[2]->Real) < (enemyMaxHp * hpThreshold + 250)) {
+                    argv[2]->Real = Math.Max((enemyHp - enemyMaxHp * hpThreshold) - 250, 0);
+                }
             } else if (enemyHp - argv[2]->Real < 250) {
+                // Non bosses (and boss phase 2s) can go to 250 HP before waiting on enrage to be killable
+                // 250 instead of 1 to hopefully make the bug where sometimes this lock could be broken
+                // by enough damage instances in the same frame.
                 argv[2]->Real = Math.Max(enemyHp - 250, 0);
             }
         }
@@ -154,7 +165,7 @@ public unsafe class Mod : IMod {
         CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
     ) {
         this.enraged = false;
-        this.isBossP1 = false;
+        this.currentEncounter = argv[0]->ToString();
 
         returnValue = this.newFightHook!.OriginalFunction(self, other, returnValue, argc, argv);
         return returnValue;
@@ -164,16 +175,30 @@ public unsafe class Mod : IMod {
         CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
     ) {
         this.enraged = false;
-        this.isBossP1 = false;
+        // Change the encounter to the phase 2 version, since it doesn't automatically happen
+        // This is important for caring about p2 mechanics (aka matti mice)
+        this.currentEncounter = this.currentEncounter.Replace("0", "1");
+        this.shiraSteelCount = 0;
         returnValue = this.phaseChangeHook!.OriginalFunction(self, other, returnValue, argc, argv);
         return returnValue;
     }
 
-    private RValue* BossStartDetour(
-        CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv
-    ) {
-        this.isBossP1 = true;
-        returnValue = this.bossStartHook!.OriginalFunction(self, other, returnValue, argc, argv);
+    private RValue* SteelActivateDetour(CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv) {
+        this.shiraSteelCount++;
+
+        if (this.shiraSteelCount == 5 || this.shiraSteelCount == 6) {
+            return returnValue;
+        }
+        returnValue = this.steelActivateHook!.OriginalFunction(self, other, returnValue, argc, argv);
+        return returnValue;
+    }
+
+    private RValue* SteelDetour(CInstance* self, CInstance* other, RValue* returnValue, int argc, RValue** argv) {
+        // If this is after the 3rd time she casts steel yourself, we've hit soft enrage
+        if (this.shiraSteelCount >= 5) {
+            this.enraged = true;
+        }
+        returnValue = this.steelHook!.OriginalFunction(self, other, returnValue, argc, argv);
         return returnValue;
     }
 
